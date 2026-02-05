916299fdd8a4f70e68593d2e6b947288ae65d284
Himanshu2005G <h04911943@gmail.com>
2026-02-05 11:59:08 +0530

    header correction

--- auth.py
+++ auth.py
@@ -2,16 +2,10 @@ from fastapi import Header, HTTPException
 
 API_KEY = "mysecretkey"
 
-def verify_api_key(
-    x_api_key: str = Header(None),
-    authorization: str = Header(None)
-):
-    if x_api_key == API_KEY:
-        return x_api_key
-
-    if authorization:
-        token = authorization.replace("Bearer ", "")
-        if token == API_KEY:
-            return token
-
-    raise HTTPException(status_code=401, detail="Not authenticated")
+def verify_api_key(x_api_key: str = Header(...)):
+    if x_api_key != API_KEY:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid API Key"
+        )
+    return x_api_key
